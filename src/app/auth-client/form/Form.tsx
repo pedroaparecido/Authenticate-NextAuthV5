ffbe4b51e32556659af7f87d21a96cf818557934
Pedro Ap <pedroaparecidori@gmail.com>
2024-08-26 15:31:46 -0300

    autenticação com client, feito dentro do componente form

--- src/app/auth-client/form/Form.tsx
+++ src/app/auth-client/form/Form.tsx
@@ -1,4 +1,6 @@
+import { AuthError } from "next-auth"
 import { FieldValues, useForm } from "react-hook-form"
+import { signIn } from "next-auth/react"
 
 
 export interface UserForm {
@@ -11,13 +13,24 @@ export default function Form() {
     const { handleSubmit, register } = useForm()
     
     const handleAuth = async (data: UserForm) => {
-
+        try {
+            const email = data.email
+            const password = data.password
+            signIn('credentials', { email, password, redirect: true, callbackUrl: '/' })
+        } catch (err) {
+            if (err instanceof AuthError) {
+                if (err.type === 'CredentialsSignin') {
+                    err.message = 'Credenciais fornecidas não encontradas'
+                    throw err
+                }
+            }
+        }
     }
 
     return(
         <form onSubmit={handleSubmit(handleAuth)} className="flex flex-col items-center justify-center gap-2">
             <input className="p-[10px] outline-none rounded-xl bg-stone-400 text-white shadow-xl" type="email" {...register('email')} />
-            <input className="p-[10px] outline-none rounded-xl bg-stone-400 text-white shadow-xl" type="text" {...register('password')} />
+            <input className="p-[10px] outline-none rounded-xl bg-stone-400 text-white shadow-xl" type="password" {...register('password')} />
             <button className="p-[20px] bg-neutral-500 text-white rounded-2xl mt-[10px] w-[230px] shadow-xl">Entrar</button>
         </form>
     )
